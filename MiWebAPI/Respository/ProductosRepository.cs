7fcca7ac4d27d83cd41a397fb5a933322f657849
VillafañeNahuel <151423052+Balaturdar@users.noreply.github.com>
2024-11-04 13:16:12 -0300

    repository productos finalizado

--- MiWebAPI/Respository/ProductosRepository.cs
+++ MiWebAPI/Respository/ProductosRepository.cs
@@ -2,38 +2,42 @@ using System.ComponentModel;
 using Microsoft.Data.Sqlite;
 
 public class ProductosRepository{
-    string CadenaDeConexion;
+    private string CadenaDeConexion;
 
     ProductosRepository()
     {
         CadenaDeConexion = "Data Source=db/Tienda.db";
     }
 
-    /*
-
-        Crear un nuevo Producto. (recibe un objeto Producto)
-        Modificar un Producto existente. (recibe un Id y un objeto Producto)
-        Listar todos los Productos registrados. (devuelve un List de Producto)
-        Obtener detalles de un Productos por su ID. (recibe un Id y devuelve un
-        Producto)
-        Eliminar un Producto por ID
-
-    */
-
     public void CrearProducto(Productos producto){
         using(SqliteConnection connection = new SqliteConnection(CadenaDeConexion)){
             string queryString = @"INSERT INTO Productos (Descripcion, Precio) 
-                                VALUES ('Mouse Inalámbrico Logitech', 5000);
+                                VALUES (@descripcion, @precio);
                                 ";
             connection.Open();
             var command = new SqliteCommand( queryString, connection);
+
+            command.Parameters.AddWithValue("@precio", producto.Precio);
+            command.Parameters.AddWithValue("@descripcion", producto.Descripcion);
+
             command.ExecuteNonQuery();
             connection.Close();
         }
     }
 
     public void ModProducto(int id, Productos producto){
+        string queryString = @"UPDATE Productos SET Descripcion = @Descripcion, Precio = @Precio WHERE idProducto = @Id";
 
+        using (SqliteConnection connection = new SqliteConnection(CadenaDeConexion))
+        {
+            connection.Open();
+            SqliteCommand command = new SqliteCommand(queryString,connection);
+            command.Parameters.AddWithValue("@Descripcion", producto.Descripcion);
+            command.Parameters.AddWithValue("@Precio", producto.Precio);
+            command.Parameters.AddWithValue("@Id", id);
+            command.ExecuteNonQuery();
+            connection.Close();            
+        }
     }
 
     public List<Productos> ListarProductos(){
@@ -60,15 +64,13 @@ public class ProductosRepository{
         return productos;
     }
 
-//Obtener detalles de un Productos por su ID.
     public Productos DetalleProductoId(int id){
         var producto = new Productos();
         using(SqliteConnection connection = new SqliteConnection(CadenaDeConexion)){
             string queryString = @"SELECT idProducto, Descripcion, Precio From Productos WHERE idProductos = @id";
             var command = new SqliteCommand( queryString, connection);
             connection.Open();
-            command.Parameters.Add("@id", SqliteType.Integer);
-            command.Parameters["@ID"].Value = id;
+            command.Parameters.AddWithValue("@id", id);
             using(SqliteDataReader reader = command.ExecuteReader())
             {
                 reader.Read();
@@ -84,10 +86,19 @@ public class ProductosRepository{
 
     public void EliminarProductoId(int id){
 
+        using(SqliteConnection connection = new SqliteConnection(CadenaDeConexion)){
+            string queryString = @"DELETE FROM Productos
+                                   WHERE idProducto = @id;";
+            
+            var command = new SqliteCommand( queryString, connection);
+            connection.Open();
+            
+            command.Parameters.AddWithValue("@id", id);
+            
+            command.ExecuteNonQuery();
+
+            connection.Close();
+        }
     }
-
-
-
-
 
 }
